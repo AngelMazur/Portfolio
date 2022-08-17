e3fde677bf859f657ccf6c01913ccdf8cfbfb8df
AngelMazur <stanlpa3.0@gmail.com>
2022-08-17 10:26:46 +0200

    fix: locations

--- Components/Box/index.js
+++ Components/Box/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import style from './Box.module.css'
+import Link from 'next/link'
 
-const Box = ({ selected, render }) => {
+const Box = ({ geometry, name, province, url }) => {
   return (
     <>
-      <div>{selected}</div>
       <ul className={style.container}>
         <li className={style.box}>
-          <p>{render}</p>
+          <Link href={url}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer">
+              <h2>{name}</h2>
+            </a>
+          </Link>
+          <h3>{province}</h3>
+          <p>{geometry}</p>
         </li>
       </ul>
     </>
